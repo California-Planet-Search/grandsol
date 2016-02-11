61ceb86acd5116ec3000be7c447f7a5a050016a7
BJ Fulton <bjfultn@gmail.com>
2016-02-11 11:17:00 -1000

    bug fixes

--- py/grandsol/driver.py
+++ py/grandsol/driver.py
@@ -2,25 +2,34 @@ import subprocess
 import os
 import shutil
 import grandsol
+import time
 
 def execute(cmd, cwd, verbose=True):
     """
     A thin wrapper for subprocess.Popen
     """
     os.chdir(cwd)
+    o = int(cmd.split()[3])
     if verbose: print "Running command: '%s'" % cmd
+    lock = subprocess.Popen(["echo 'Start Time: '`date` > order_%02d.run" % o], shell=True)
+
     p = subprocess.Popen(cmd.split())
     stdout, stderr = p.communicate()
     
-def run_orders(runname, obslist, ppserver=None, orders=[3,4,5,6,7,8,9]):
+    lock = subprocess.Popen(["echo 'End Time: '`date` >> order_%02d.run" % o], shell=True)
+    time.sleep(1)
+    lock = subprocess.Popen(["mv order_%02d.run order_%02d.done" % (o,o)], shell=True)
+    
+def run_orders(runname, obslist, ppserver=None, overwrite=False, orders=[1,2,3,4,5,6,7,8,9,10,11,12]):
     jobs = []
     for o in orders:
         cwd = os.getcwd()
         cmd = "grand %s %s %d 111111 out=%s.%02d.log" % (obslist, runname, o, runname, o)
-        if ppserver == None:
-            execute(cmd, cwd)
-        else:
-            jobs.append(ppserver.submit(execute, (cmd,cwd), modules=('subprocess','os')))
+        if overwrite or not os.path.isfile('order_%02d.done' % o): 
+            if ppserver == None:
+                execute(cmd, cwd)
+            else:
+                jobs.append(ppserver.submit(execute, (cmd,cwd), modules=('subprocess','os', 'time')))
 
     for j in jobs:
         j()
@@ -30,7 +39,7 @@ def run_orders(runname, obslist, ppserver=None, orders=[3,4,5,6,7,8,9]):
 def run_iterations(opt, ppserver=None):
     df = grandsol.io.get_observations(opt.star)
     runname = "iGrand_" + opt.star
-    idir = os.getcwd()
+    rundir = os.getcwd()
     
     for i in range(opt.niter):
         n = i+1
@@ -43,7 +52,7 @@ def run_iterations(opt, ppserver=None):
         else:
             obdf = grandsol.io.write_obslist(df, opt.sysvel, outfile=obfile, vorb=vdf['mnvel'].values)  # Need to update vorb here
 
-        run_orders(runname, obfile, ppserver, orders=opt.orders)
+        run_orders(runname, obfile, ppserver, orders=opt.orders, overwrite=opt.overwrite)
         vdf = grandsol.io.combine_orders(runname, obdf, opt.orders)
         
-        os.chdir(idir)
+        os.chdir(rundir)
