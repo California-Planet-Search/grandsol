039a492299e307c1bb3ec6d26de7b26d0d1dd1f6
BJ Fulton <bjfultn@gmail.com>
2016-05-06 10:26:49 -1000

    adding wlsexplorer.py

---
+++ bin/wlsexplorer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pylab as pl
+import pandas as pd
+import os
+import sys
+import subprocess
+import argparse
+
+import grandsol
+
+
+def args():
+    """
+    Parse command line arguments.
+    """
+    
+    parser = argparse.ArgumentParser(description='Plot wavelength solutions output by grand and compare to the input truth')
+    parser.add_argument(metavar='runname',dest='runname',action='store',
+                        help='Name of the iGrand run (e.g. iGrand_sun or iGrand_4628)', type=str)
+    parser.add_argument(metavar='obs',dest='obs',action='store',
+                        help='observation index from obslist (starting with 1, not 0)', type=int)
+    parser.add_argument(metavar='order',dest='order',action='store',
+                        help='order index (starting with 1, not 0)', type=int)
+    parser.add_argument('-s','--show',dest='show',action='store_true',
+                        help='display the interactive plotting window')
+
+
+    opt = parser.parse_args()
+
+    return opt
+    
+def main():
+    opt = args()
+
+    fig = grandsol.plotting.plot_lsf_byiter(opt.runname, opt.obs, opt.order)
+
+    pl.savefig("%s_%03d_%02d_lsf_byiter.png" % (opt.runname, opt.obs, opt.order))
+    if opt.show: pl.show()
+
+if __name__ == '__main__':
+    main()
